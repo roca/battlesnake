8c1bf5528b9adbcca1933b94798b05c83d58835f
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2021-06-16 13:18:50 -0400

    If futureHead == self current tail this should be safe

--- strategies/avoidsnakes.go
+++ strategies/avoidsnakes.go
@@ -4,8 +4,16 @@ import "github.com/roca/battlesnake/types"
 
 func AvoidSnakes(board types.Board, you types.Battlesnake, next_move string) bool {
 	futureHead := predict_future_position(you.Head, next_move)
+	// If futureHead == self current tail
+	// this should be safe
+	selfCurrentTail := you.Body[you.Length-1]
 
 	for _, snake := range board.Snakes {
+
+		if futureHead.X == selfCurrentTail.X && futureHead.Y == selfCurrentTail.Y {
+			return true
+		}
+
 		if !avoidSnake(futureHead, snake) {
 			return false
 		}
@@ -15,7 +23,6 @@ func AvoidSnakes(board types.Board, you types.Battlesnake, next_move string) boo
 }
 
 func avoidSnake(futureHead types.Coord, snake types.Battlesnake) bool {
-
 	for _, coord := range snake.Body {
 		if futureHead.X == coord.X && futureHead.Y == coord.Y {
 			return false
