a8eef770c7daf48dbb863b5dac4d611145f79290
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2021-06-17 15:03:24 -0400

    avoid_self_2: wanted to compete

--- strategies/nextmove.go
+++ strategies/nextmove.go
@@ -8,6 +8,31 @@ import (
 	"github.com/roca/battlesnake/types"
 )
 
+func AvoidSelfWallsFood(game types.GameRequest) string {
+	//Choose a random direction to move in
+	possibleMoves := []string{"up", "down", "left", "right"}
+	move := possibleMoves[rand.Intn(len(possibleMoves))]
+	for i := 0; i < 4; i++ {
+
+		safe := AvoidSelf(game.You, move) &&
+			AvoidWalls(game.Board, game.You, move) &&
+			AvoidFood(game.Board, game.You, move)
+		fmt.Println(move, "Attempt", i, "AvoidSnakesAndWalls:", safe)
+		if safe {
+			break
+		}
+		// Drop unsafe move from possible moves
+		possibleMoves = filter.Drop(possibleMoves, func(m string) bool {
+			return m == move
+		}).([]string)
+		if len(possibleMoves) == 0 {
+			break
+		}
+		move = possibleMoves[rand.Intn(len(possibleMoves))]
+	}
+	return move
+}
+
 func AvoidSnakesAndWalls(game types.GameRequest) string {
 	//Choose a random direction to move in
 	possibleMoves := []string{"up", "down", "left", "right"}
@@ -29,7 +54,6 @@ func AvoidSnakesAndWalls(game types.GameRequest) string {
 		move = possibleMoves[rand.Intn(len(possibleMoves))]
 	}
 	return move
-
 }
 
 func AvoidSnakesWallsAndFood(game types.GameRequest) string {
