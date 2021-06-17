43eae4e4fa94195e696dc7c0023f6ac93f2641fe
Romel Campbell <RomelCampbell@gmail.com>
2021-06-17 16:02:35 +0000

    Experimenting with health and length

--- strategies/avoidFood.go
+++ strategies/avoidFood.go
@@ -5,7 +5,9 @@ import "github.com/roca/battlesnake/types"
 func AvoidFood(board types.Board, you types.Battlesnake, next_move string) bool {
 	futureHead := predict_future_position(you.Head, next_move)
 	for _, coord := range board.Food {
-		if futureHead.X == coord.X && futureHead.Y == coord.Y && you.Health > 20 {
+		if  futureHead.X == coord.X && 
+        futureHead.Y == coord.Y && 
+        (you.Health > 100 || you.Length > 10) {
 			return false
 		}
 	}
