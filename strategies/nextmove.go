a21f6a9307465149a751b4d30eecdbeb6d1af60a
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2021-06-16 20:48:55 -0400

    Avoid food first try

--- strategies/nextmove.go
+++ strategies/nextmove.go
@@ -14,7 +14,33 @@ func AvoidSnakesAndWalls(game types.GameRequest) string {
 	move := possibleMoves[rand.Intn(len(possibleMoves))]
 	for i := 0; i < 4; i++ {
 
-		safe := AvoidSnakes(game.Board, game.You, move) && AvoidWalls(game.Board, game.You.Head, move)
+		safe := AvoidSnakes(game.Board, game.You, move) && AvoidWalls(game.Board, game.You, move)
+		fmt.Println(move, "Attempt", i, "AvoidedSnakes:", safe)
+		if safe {
+			break
+		}
+		// Drop unsafe move from possible moves
+		possibleMoves = filter.Drop(possibleMoves, func(m string) bool {
+			return m == move
+		}).([]string)
+		if len(possibleMoves) == 0 {
+			break
+		}
+		move = possibleMoves[rand.Intn(len(possibleMoves))]
+	}
+	return move
+
+}
+
+func AvoidSnakesWallsAndFood(game types.GameRequest) string {
+	//Choose a random direction to move in
+	possibleMoves := []string{"up", "down", "left", "right"}
+	move := possibleMoves[rand.Intn(len(possibleMoves))]
+	for i := 0; i < 4; i++ {
+
+		safe := AvoidSnakes(game.Board, game.You, move) &&
+			AvoidWalls(game.Board, game.You, move) &&
+			AvoidFood(game.Board, game.You, move)
 		fmt.Println(move, "Attempt", i, "AvoidedSnakes:", safe)
 		if safe {
 			break
