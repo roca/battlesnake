f2ee6497fc5e20132ca91d79ad24abe1e0771e04
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2021-06-17 15:08:19 -0400

    avoid_self_2: wanted to compete

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ func HandleMove(w http.ResponseWriter, r *http.Request) {
 	}
 
 	response := types.MoveResponse{
-		Move: strategies.AvoidSnakesWallsAndFood(request),
+		Move: strategies.AvoidSelfWallsFood(request),
 	}
 
 	fmt.Printf("MOVE: %s\n", response.Move)
