63a1376091b72b041d2caf7a7167363c32e31846
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2021-06-18 16:42:30 -0400

    testing failure

--- test/unit/nextmove_test.go
+++ test/unit/nextmove_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestSanity(t *testing.T) {
 	expected := 1.0
-	actual := 1.0
+	actual := 1.2
 	assert.Equalf(t, expected, actual, "TestSanity: expected %f != actual %f",expected,actual)
 
 }
