cc2462403673f1c088adb80eae9dc84bb130393f
ROMEL CAMPBELL <RomelCampbell@gmail.com>
2021-06-18 16:53:48 -0400

    Makimg test pass

--- test/unit/nextmove_test.go
+++ test/unit/nextmove_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestSanity(t *testing.T) {
 	expected := 1.0
-	actual := 1.2
+	actual := 1.0
 	assert.Equalf(t, expected, actual, "TestSanity: expected %f != actual %f",expected,actual)
 
 }
